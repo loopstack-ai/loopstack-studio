808256ca27b93ea4d7df3e1dd4b9e14b7ce9783d
Tobias Blaettermann <tobias.blaettermann1@gmail.com>
2025-11-17 14:43:29 +0100

    feat: add 2 build-modes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // Public API exports
-export * from './types';
-export * from './hooks';
-export * from './services';
-export * from './providers/StudioProvider';
+export * from '@/types';
+export * from '@/hooks';
+export * from '@/services';
+export * from '@/providers/StudioProvider';
+export * from '@/routing/LocalRouter';
 
-export { default as DebugPage } from './pages/DebugPage';
+export { default as DebugPage } from '@/pages/DebugPage';
